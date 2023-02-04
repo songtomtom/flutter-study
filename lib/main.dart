0d700b0d20cc01363828e89dd196eda99c66b8f5
songtomtom <tom.song@luxrobo.com>
2023-02-04 23:52:18 +0900

    픽스 다트 const

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Player {
-  String name;
-
-  Player({required this.name});
-}
-
 void main() {
-  var nico = Player(name: 'potato');
   runApp(const App());
 }
 
